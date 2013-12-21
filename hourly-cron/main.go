f61f404874aa404d357c11e78b40a32559d8c2e5
James Dunne <james.jdunne@gmail.com>
2013-12-21 03:52:36 -0600

    Storing extra historical information for later analysis.

--- hourly-cron/main.go
+++ hourly-cron/main.go
@@ -85,8 +85,13 @@ type QuoteResponse struct {
 }
 
 type Historical struct {
-	Date  string
-	Close string
+	Symbol string
+	Date   string
+	Open   string
+	Close  string
+	High   string
+	Low    string
+	Volume string
 }
 
 type HistoricalResponse struct {
@@ -154,8 +159,8 @@ create table if not exists stock_track (
 
 	if _, err = db.Exec(`
 create index if not exists stock_track_ix on stock_track (
-	symbol ASC,
-	purchaser_email ASC
+	purchaser_email ASC,
+	symbol ASC
 )`); err != nil {
 		db.Close()
 		return
@@ -164,9 +169,13 @@ create index if not exists stock_track_ix on stock_track (
 	if _, err = db.Exec(`
 create table if not exists stock_history (
 	symbol TEXT NOT NULL,
-	closing_date TEXT NOT NULL,
+	date TEXT NOT NULL,
+	opening_price TEXT NOT NULL,
 	closing_price TEXT NOT NULL,
-	CONSTRAINT stock_history_pk PRIMARY KEY (symbol, closing_date)
+	lowest_price TEXT NOT NULL,
+	highest_price TEXT NOT NULL,
+	volume INTEGER NOT NULL,
+	CONSTRAINT stock_history_pk PRIMARY KEY (symbol, date)
 )`); err != nil {
 		db.Close()
 		return
@@ -175,7 +184,7 @@ create table if not exists stock_history (
 	if _, err = db.Exec(`
 create index if not exists stock_history_ix on stock_history (
 	symbol ASC,
-	closing_date DESC
+	date DESC
 )`); err != nil {
 		db.Close()
 		return
@@ -184,7 +193,7 @@ create index if not exists stock_history_ix on stock_history (
 	// Add some test data:
 	db.Execl(`
 insert or ignore into stock_track (symbol, purchaser_email, purchase_price, purchase_date, trailing_stop_percent)
-            values ('MSFT', 'email@example.org', '30.00', '2013-12-01', '20.00')`)
+            values ('MSFT', 'email@example.org', '30.00', '2013-12-01', '2')`)
 
 	return
 }
@@ -195,12 +204,6 @@ type dbStock struct {
 	PurchaseDate        string `db:"purchase_date"`
 	PurchaserEmail      string `db:"purchaser_email"`
 	TrailingStopPercent string `db:"trailing_stop_percent"`
-}
-
-type dbStockHistory struct {
-	Symbol       string `db:"symbol"`
-	ClosingDate  string `db:"closing_date"`
-	ClosingPrice string `db:"closing_price"`
 }
 
 // ------------- main:
@@ -230,34 +233,26 @@ from stock_track as s`); err != nil {
 		log.Fatalln(err)
 	}
 
-	now := time.Now()
+	today := toDate(time.Now().In(nyLoc))
 	fmt.Print("\n")
 	for _, st := range stocks {
 		//fmt.Printf("  %#v\n", st)
 
 		purchaseDate, _ := time.Parse(dateFmt, st.PurchaseDate)
-		fmt.Printf("'%s' purchased by <%s> on %s for %s\n", st.Symbol, st.PurchaserEmail, purchaseDate.Format(dateFmt), toRat(st.PurchasePrice).FloatString(2))
+		fmt.Printf("'%s' purchased by <%s> on %s for %s with %s%% trailing stop\n", st.Symbol, st.PurchaserEmail, purchaseDate.Format(dateFmt), toRat(st.PurchasePrice).FloatString(2), toRat(st.TrailingStopPercent).FloatString(2))
 
 		// Stock times/dates are in NYC timezone:
-		yesterday := toDate(now.Add(time.Duration(time.Hour * 24 * -1)).In(nyLoc))
+		yesterday := today.Add(time.Duration(time.Hour * 24 * -1))
 
 		// Determine the last-fetched date for the stock:
 		var lastDate time.Time
-		lastDateRec := new(dbStockHistory)
-		err = db.Get(lastDateRec, `
-select h.symbol, h.closing_date, h.closing_price
-from stock_history as h
-where date(h.closing_date) = (select max(date(closing_date)) from stock_history where symbol = h.symbol)
-  and h.symbol = ?`, st.Symbol)
-		if err == sql.ErrNoRows {
+		ld, err := dbGetScalar(db, `select h.date from stock_history h where (h.symbol = ?) and (datetime(h.date) = (select max(datetime(date)) from stock_history where symbol = h.symbol))`, st.Symbol)
+		if ld == nil {
 			// No row; use 7 days ago in NYC time:
-			lastDate = toDate(now.Add(time.Duration(time.Hour * 24 * -7)).In(nyLoc))
-		} else if err != nil {
-			log.Println(err)
-			continue
+			lastDate = today.Add(time.Duration(time.Hour * 24 * -7))
 		} else {
-			// Extract the last-fetched date from the db record in NYC time:
-			lastDate, _ = time.ParseInLocation(time.RFC3339, lastDateRec.ClosingDate, nyLoc)
+			// Extract the last-fetched date from the db record, assuming NYC time:
+			lastDate, _ = time.ParseInLocation(time.RFC3339, ld.(string), nyLoc)
 			lastDate = toDate(lastDate)
 		}
 		//fmt.Println(yesterday.Format("2006-01-02 15:04:05 -0700"))
@@ -271,7 +266,7 @@ where date(h.closing_date) = (select max(date(closing_date)) from stock_history 
 			fmt.Printf("  Fetching historical data since %s...\n", lastDate.Format(dateFmt))
 
 			// TODO(jsd): YQL parameter escaping!
-			yqlHistQuery := `select Date, Close from yahoo.finance.historicaldata where symbol = "` + st.Symbol + `" and startDate = "` + lastDate.Format(dateFmt) + `" and endDate = "` + yesterday.Format(dateFmt) + `"`
+			yqlHistQuery := `select Symbol, Date, Open, Close, High, Low, Volume from yahoo.finance.historicaldata where symbol = "` + st.Symbol + `" and startDate = "` + lastDate.Format(dateFmt) + `" and endDate = "` + yesterday.Format(dateFmt) + `"`
 
 			hist := new(HistoricalResponse)
 			if err = yql(hist, yqlHistQuery); err != nil {
@@ -282,24 +277,34 @@ where date(h.closing_date) = (select max(date(closing_date)) from stock_history 
 
 			// Insert historical records into the DB:
 			fmt.Printf("  Recording...\n")
-			for _, value := range hist.Query.Results.Quote {
+			for _, q := range hist.Query.Results.Quote {
 				// Store dates as RFC3339 in the NYC timezone:
-				date, err := time.ParseInLocation(dateFmt, value.Date, nyLoc)
+				date, err := time.ParseInLocation(dateFmt, q.Date, nyLoc)
 				if err != nil {
 					log.Println(err)
 					continue
 				}
 
 				// Insert the history record; log any errors:
-				db.Execl(`insert into stock_history (symbol, closing_date, closing_price) values (?,?,?)`, st.Symbol, date.Format(time.RFC3339), value.Close)
+				db.Execl(
+					`insert into stock_history (symbol, date, opening_price, closing_price, highest_price, lowest_price, volume) values (?,?,?,?,?,?,?)`,
+					st.Symbol,
+					date.Format(time.RFC3339),
+					q.Open,
+					q.Close,
+					q.High,
+					q.Low,
+					q.Volume,
+				)
 			}
 			fmt.Printf("  Recorded.\n")
 		}
 
-		// Get the current stock price:
+		// Get the current stock price from Yahoo! Finance:
 		var currPrice *big.Rat
-		// TODO(jsd): Disable this `if false` business for release.
-		if false {
+
+		// TODO(jsd): Remove this `if true/false` business for release.
+		if true {
 			fmt.Printf("  Fetching current trading price...\n")
 			quot := new(QuoteResponse)
 			err = yql(quot, `select LastTradePriceOnly from yahoo.finance.quote where symbol = "`+st.Symbol+`"`)
